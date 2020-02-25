8fd21fc6325c125318fde1a5e07872814d38e780
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2020-02-25 11:17:40 +0200

    cleanup

--- src/scripts/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/swHostBoot.js', {scope: "/"}).then(function(reg) {
-        console.log('Yay, service worker is live!', reg);
-    }).catch(function(err) {
-        console.log('No oats for you.', err);
-    });
-}
