c0298c94e9ec94f23ee17d27f86b19cc8fc2d890
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-21 16:22:58 +0300

    Added initial PWA install version.

---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: "./",
+  globPatterns: ["**/*.{html,js,css,ttf,eot,svg,woff,woff2,jpg,png}"],
+  globIgnores: ["sw*.js", "node_modules/**/*.*"],
+  swDest: "./swPwa.js",
+};
