b4c01c35eed8c183cefa4a93c7833bc72936d226
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-11-22 15:40:51 +0200

    Added reload button to UIService loader after 5 seconds.

--- controllers/services/UIService.js
+++ controllers/services/UIService.js
@@ -1,3 +1,5 @@
+import SWAgent from "./SWAgent.js";
+
 const SpinnerHTML = "<div class=\"loader-container\">\n" +
     "<div class=\"sk-cube-grid\">\n" +
     "    <div class=\"sk-cube sk-cube1\"></div>\n" +
@@ -12,18 +14,55 @@ const SpinnerHTML = "<div class=\"loader-container\">\n" +
     "</div>\n" +
     "</div>";
 
+const RELOAD_SECTION_TIMEOUT_MS = 5 * 1000;
+const RELOAD_SECTION_HTML = `
+    <p>
+      The application is taking longer than expected to load. <br/>
+      If you have network issues please use the following to refresh the application.
+    </p>
+    <button>Refresh</button>
+`;
+
 function Spinner(view) {
 
     let attachedSpinner = null;
+    let reloadSectionTimeout = null;
+
     this.attachToView = function () {
         let element = document.createElement("div");
         attachedSpinner = view.appendChild(element);
         attachedSpinner.innerHTML = SpinnerHTML;
+
+        reloadSectionTimeout = setTimeout(() => {          
+          if(!attachedSpinner) {
+            // the spinner has been removed already
+            return;
+          }
+          if("contains" in document.body && !document.body.contains(attachedSpinner)) {
+            // the spinner has been replaced by something else
+            return;
+          }
+
+          let reloadSectionElement = document.createElement("div");
+          reloadSectionElement.className = "reload-section";
+          const reloadSection = attachedSpinner.querySelector(".loader-container").appendChild(reloadSectionElement);
+          reloadSection.innerHTML = RELOAD_SECTION_HTML;
+  
+          reloadSection.querySelector("button").addEventListener("click", () => {
+            SWAgent.unregisterAllServiceWorkers(() => {
+              window.location.reload();
+            });
+          });
+        }, RELOAD_SECTION_TIMEOUT_MS);
     };
 
     this.removeFromView = function () {
         if (attachedSpinner) {
             attachedSpinner.remove();
+            attachedSpinner = null;
+        }
+        if(reloadSectionTimeout) {
+          clearTimeout(reloadSectionTimeout);
         }
     }
 }
