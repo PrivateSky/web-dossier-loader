2d2630d860d73cdc7c077c962570cacceea8a843
salboaie <salboaie@gmail.com>
2020-12-01 18:23:22 +0200

    cleaner names

--- config-constants.js
+++ config-constants.js
@@ -1,11 +1,11 @@
 let APP_CONFIG = {
 	THEME: "app",
 	LABELS_DICTIONARY: {
-		APP_NAME: "Web wallet",
-		APP_DESCRIPTION: "A smart application to keep your secrets",
-		NEW_WALLET: "New Wallet",
-		ACCESS_WALLET: "Access Wallet",
-		WALLET_AUTHORIZATION: "Wallet Authorization",
+		APP_NAME: "DSU Fabric",
+		APP_DESCRIPTION: "The \"backend\" application for EPI",
+		NEW_WALLET: "New Account",
+		ACCESS_WALLET: "Access Account",
+		WALLET_AUTHORIZATION: "Authorization",
 		REGISTER_DETAILS: "Register details",
 		COMPLETE: "Complete",
 
@@ -18,7 +18,7 @@ let APP_CONFIG = {
 		ENTER_EMAIL: "Email",
 
 		SET_UP_PASSWORD: "Enter your password",
-		SET_UP_PASSWORD_HELP: "Password should have at least 4 characters",
+		SET_UP_PASSWORD_HELP: "Password min. 12 chars including 1xUpper char, 1xDigit, 1xSpecial char",
 		ENTER_PASSWORD: "Password",
 
 		SET_UP_CONFIRM_PASSWORD: "Confirm your password",
@@ -30,7 +30,7 @@ let APP_CONFIG = {
 		REGISTER_SUCCESSFULLY: "Register successfully",
 
 		ENTER_CREDENTIALS: "Enter your credentials",
-		OPEN_WALLET: "Open Wallet",
+		OPEN_WALLET: "Enter",
 		SEED: "Seed",
 		ENTER_WALLET_SEED: "Enter Wallet Seed",
 		SEED_PRINT: "You can print it on a piece of paper.",
@@ -46,14 +46,15 @@ let APP_CONFIG = {
 	NEW_OR_RESTORE_CONTAINER_ID: "restore-new-container",
 	PASSWORD_CONTAINER_ID: "credentials-container",
 	MODE: 'secure',
-	PASSWORD_MIN_LENGTH: 4,
+	PASSWORD_MIN_LENGTH: 12,
 	USERNAME_MIN_LENGTH: 6,
 	USERNAME_REGEX: /^[a-zA-Z]([A-Za-z0-9]+[\\._]{0,1}[A-Za-z0-9]+){2,10}$/,
 	EMAIL_REGEX: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+	PASSWORD_REGEX: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).*$/,
 	NEW_WALLET_MORE_INFORMATION: `<div class="jumbotron p-0 m-0" align="center">
-  <h1 class="display-6">Welcome to your secure wallet!</h1>
-  <p class="lead">After completing the following wizard you will gain access to your private data.</p>
-  <p class="m-0">In order to access your private data you have to set up your credentials.</p>
+  <h1 class="display-6">Welcome to EPI backend app!</h1>
+  <p class="lead">After completing the following wizard you will gain access to EPI backend app.</p>
+  <p class="m-0">In order to gain access you have to set up your credentials.</p>
   <hr/>
 </div>`
 };
